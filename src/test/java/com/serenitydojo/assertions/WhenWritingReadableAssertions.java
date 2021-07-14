d3ce8502deb9d3ea19c9271e1a8cee838c9276a5
Saif <sai5i.islam@gmail.com>
2021-07-14 11:52:48 +0500

    added Assertj tests to WhenWritingReadableAssertions.java

--- src/test/java/com/serenitydojo/assertions/WhenWritingReadableAssertions.java
+++ src/test/java/com/serenitydojo/assertions/WhenWritingReadableAssertions.java
@@ -2,11 +2,12 @@ package com.serenitydojo.assertions;
 
 import org.assertj.core.data.Percentage;
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withinPercentage;
 
 public class WhenWritingReadableAssertions {
 
@@ -17,7 +18,7 @@ public class WhenWritingReadableAssertions {
         int c = 20;
         int sum = a + b + c;
 
-        // Check that the sum is 35
+        assertThat(sum).isEqualTo(35); // Check that the sum is 35
     }
 
     @Test
@@ -27,7 +28,7 @@ public class WhenWritingReadableAssertions {
         int c = 20;
         int sum = a + b + c;
 
-        // Check that the sum is greater than 30
+        assertThat(sum).isGreaterThan(30);// Check that the sum is greater than 30
     }
 
     @Test
@@ -36,49 +37,49 @@ public class WhenWritingReadableAssertions {
         int b = 10;
         int sum = a + b;
 
-        // Check that the sum is between 10 and 20
+        assertThat(sum).isBetween(10,20);// Check that the sum is between 10 and 20
     }
 
     @Test
     public void whenANumberIsCloseToAnotherValue() {
         double roughlyOneHundred = 99;
 
-        // Check that the sum is within 10% of 100
+        assertThat(roughlyOneHundred).isCloseTo(100,withinPercentage(10));// Check that the sum is within 10% of 100
     }
 
     @Test
     public void whenAStringIsEqualToAnother() {
         String color = "red";
 
-        // Check that the string has a value of 'red'
+        assertThat(color).isEqualTo("red");// Check that the string has a value of 'red'
     }
 
     @Test
     public void whenAStringIsEqualIgnoringCase() {
         String color = "RED";
 
-        // Check that the string has a value of 'red' (ignoring case)
+        assertThat(color).isEqualToIgnoringCase("red");// Check that the string has a value of 'red' (ignoring case)
     }
 
     @Test
     public void whenAStringContainsOnlyDigits() {
         String licenseNumber = "12345";
 
-        // Check that the value contains only digits
+        assertThat(licenseNumber).containsOnlyDigits();// Check that the value contains only digits
     }
 
     @Test
     public void whenAStringStartsWithAValue() {
         String colors = "red,green,blue";
 
-        // Check that the value starts with 'red'
+        assertThat(colors).startsWith("red");// Check that the value starts with 'red'
     }
 
     @Test
     public void whenAStringEndsWithAValue() {
         String colors = "red,green,blue";
 
-        // Check that the value ends with 'blue'
+        assertThat(colors).endsWith("blue");// Check that the value ends with 'blue'
     }
 
 
@@ -86,35 +87,35 @@ public class WhenWritingReadableAssertions {
     public void whenAListContainsAValue() {
         List<String> colors = Arrays.asList("red","green","blue");
 
-        // Check that the list contains "red"
+        assertThat(colors).contains("red");// Check that the list contains "red"
     }
 
     @Test
     public void whenAListContainsASetOfValues() {
         List<String> colors = Arrays.asList("red","green","blue");
 
-        // Check that the list contains "red" and "blue
+        assertThat(colors).contains("red","blue");// Check that the list contains "red" and "blue
     }
 
     @Test
     public void whenAListHasASpecificSize() {
         List<String> colors = Arrays.asList("red","green","blue");
 
-        // Check that the list contains 3 elements
+        assertThat(colors).containsExactly("red","green","blue");// Check that the list contains 3 elements
     }
 
     @Test
     public void whenAListContainsExactlyASetOfValues() {
         List<String> colors = Arrays.asList("red","green","blue");
 
-        // Check that the list contains "red","green" and "blue" in any order
+        assertThat(colors).containsExactlyInAnyOrder("red","green","blue");// Check that the list contains "red","green" and "blue" in any order
     }
 
     @Test
     public void whenAListContainsExactlyASetOfValuesInAnyOrder() {
         List<String> colors = Arrays.asList("red","green","blue");
 
-        // Check that the list contains "red","blue" and "green" in any order
+        assertThat(colors).containsExactlyInAnyOrder("red","green","blue");// Check that the list contains "red","blue" and "green" in any order
     }
 
 
